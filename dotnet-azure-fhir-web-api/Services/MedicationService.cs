332a37ecbb90369a1612bcc82cc9ed3a920f6ae2
Daiana Bassi <Daiana.Bassi@gosh.nhs.uk>
2020-08-03 15:41:36 +0100

    created vital sign endpoint and medication

--- dotnet-azure-fhir-web-api/Services/MedicationService.cs
+++ dotnet-azure-fhir-web-api/Services/MedicationService.cs
@@ -22,7 +22,9 @@ namespace dotnet_azure_fhir_web_api.Services
         public async Task<List<JObject>> GetPatientMedication(string id)
         {
             _logger.LogInfo("Class: MedicationService, Method: GetPatientMedication");
-            return await _resource.GetAllPages($"{requestOption[1]}{id}");
+            var json = await _resource.GetAllPages($"{requestOption[1]}{id}");
+
+            return GetMedicationObject(json[0]["entry"]);
         }
 
         public async Task<List<JObject>> GetPatientMedicationPages(string id, int pages)
@@ -35,6 +37,21 @@ namespace dotnet_azure_fhir_web_api.Services
         {
             _logger.LogInfo("Class: MedicationService, Method: GetSingleMedication");
             return await _resource.GetSingleResource($"{requestOption[0]}{id}");
+        }
+
+        private List<JObject> GetMedicationObject(JToken array)
+        {
+            List<JObject> result = new List<JObject>();
+            foreach(var item in array)
+            {
+                result.Add(new JObject(
+                    new JProperty("drug", item["resource"]["medicationCodeableConcept"]["text"]),
+                    new JProperty("date", item["resource"]["authoredOn"]),
+                    new JProperty("prescriber", item["resource"]["requester"]["display"])
+                    ));
+            }
+
+            return result;
         }
     }
 }
--- dotnet-azure-fhir-web-api/Services/ObservationService.cs
+++ dotnet-azure-fhir-web-api/Services/ObservationService.cs
@@ -20,7 +20,9 @@ namespace dotnet_azure_fhir_web_api.Services
         public async Task<List<JObject>> GetPatientObservations(string id)
         {
             _logger.LogInfo("Class: ObservationService, Method: GetPatientObservations");
-            return await _resource.GetAllPages($"{requestOption[1]}{id}");
+            var json = await _resource.GetAllPages($"{requestOption[1]}{id}");
+            return GetVitalSigns(json[0]["entry"]);
+
         }
 
         public async Task<List<JObject>> GetPatientObservationPages(string id, int pages)
@@ -39,6 +41,27 @@ namespace dotnet_azure_fhir_web_api.Services
         {
             _logger.LogInfo("Class: ObservationService, Method: GetMultipleObservation");
             return await _resource.GetMultipleResources($"{requestOption[0]}", ids);
+        }
+
+        private List<JObject> GetVitalSigns(JToken array)
+        {
+            List<JObject> result = new List<JObject>();
+
+            foreach(var item in array)
+            {
+                if (((string)item["resource"]["category"][0]["coding"][0]["display"]).Contains("vital-signs") && !((string)item["resource"]["code"]["text"]).Contains("Blood Pressure"))
+                {
+                    result.Add(new JObject(
+                        new JProperty("type", item["resource"]["code"]["text"]),
+                        new JProperty("date", item["resource"]["issued"]),
+                        new JProperty("value", item["resource"]["valueQuantity"]["value"]),
+                        new JProperty("unit", item["resource"]["valueQuantity"]["unit"])
+                    ));
+                }
+
+            }
+
+            return result;
         }
 
     }
