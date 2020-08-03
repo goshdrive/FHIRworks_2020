ecfd4964e42465dd9bd155bba09c04fd8758dea9
Daiana Bassi <Daiana.Bassi@gosh.nhs.uk>
2020-08-03 14:44:48 +0100

    added new patient object

--- dotnet-azure-fhir-web-api/Services/PatientService.cs
+++ dotnet-azure-fhir-web-api/Services/PatientService.cs
@@ -32,16 +32,32 @@ namespace dotnet_azure_fhir_web_api.Services
         public async Task<JObject> GetPatient(string id)
         {
             _logger.LogInfo("Class: PatientService, Method: GetPatient");
-            return await _resource.GetSingleResource($"{requestOption}{id}");
+            var json = await _resource.GetSingleResource($"{requestOption}{id}");
+            return GetPatientObject(json);
+
         }
 
-
-
-
-
-
-
-
-
+        private JObject GetPatientObject(JObject json)
+        {
+            string name = "";
+            foreach (var item in json["name"][0]["given"])
+            {
+                name = name + " " + item;
+            }
+            return new JObject(
+                new JProperty("title", $"{json["name"][0]["prefix"][0]}"),
+                new JProperty("name", $"{name}{" "}{json["name"][0]["family"]}"),
+                new JProperty("line", $"{json["address"][0]["line"][0]}"),
+                new JProperty("city", $"{json["address"][0]["city"]}"),
+                new JProperty("state", $"{json["address"][0]["state"]}"),
+                new JProperty("postcode", $"{json["address"][0]["postalCode"]}"),
+                new JProperty("country", $"{json["address"][0]["country"]}"),
+                new JProperty("ethnicity", json["extension"][0]["extension"][0]["valueCoding"]["display"]),
+                new JProperty("gender", json["gender"]),
+                new JProperty("birthDate", json["birthDate"]),
+                new JProperty("maritalStatus", json["maritalStatus"]["text"]),
+                new JProperty("language", json["communication"][0]["language"]["text"])
+                );
+        }
     }
 }
